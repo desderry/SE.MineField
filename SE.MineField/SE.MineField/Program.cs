7b1da0c6e17143f4310dfc39576de71ca7d8c991
Matt Derry <matt.derry@digital.education.gov.uk>
2021-01-23 17:54:57 +0000

    Initial commit of blank console app

---
+++ SE.MineField/SE.MineField/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SE.MineField
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
